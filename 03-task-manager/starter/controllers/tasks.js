94fad392da40ab21e28cf6ef7d591694ba508a04
FangshengXuaJason <xufangsheng@seattleu.edu>
2021-12-18 01:14:15 -0800

    Update tasks.js

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -3,7 +3,7 @@ const Task = require('../models/Task')
 const getAllTasks = async (req, res) => {
     try {
         const tasks = await Task.find({})
-        res.status(201).json({tasks:tasks})
+        res.status(201).json({tasks})
     } catch (error) {
         res.status(500).json({msg:error})
     }
