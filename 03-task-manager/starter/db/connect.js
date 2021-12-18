ff7885987eee4dce2876f803655d1c47d93f770b
FangshengXuaJason <xufangsheng@seattleu.edu>
2021-12-17 21:15:05 -0800

    Update connect.js

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,2 +1,14 @@
-const connectionString = 'mongodb+srv://FangshengXu:Xfs19970910@nodeexpressprojects.2deue.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongoose = require('mongoose')
 
+const connectionString = 'mongodb+srv://FangshengXu:Xfs19970910@nodeexpressprojects.2deue.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority'
+
+mongoose
+    .connect(connectionString, {
+        useNewUrlParser: true,
+        useCreateIndex: true, 
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+     })
+    .then(()=>console.log('CONNECTED TO THE DB'))
+    .catch((err) => console.log(err))
+    
