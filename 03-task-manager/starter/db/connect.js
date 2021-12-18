6056a54d5f471129bcec3de072fe22328d52881c
FangshengXuaJason <xufangsheng@seattleu.edu>
2021-12-17 22:24:15 -0800

    Update connect.js

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose')
 
-const connectionString = 'mongodb+srv://FangshengXu:Xfs19970910@nodeexpressprojects.2deue.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority'
-
-mongoose
-    .connect(connectionString, {
+const connectDB = (url) => {
+    return mongoose.connect(url, {
         useNewUrlParser: true,
         useCreateIndex: true, 
         useFindAndModify: false,
         useUnifiedTopology: true,
-     })
-    .then(()=>console.log('CONNECTED TO THE DB'))
-    .catch((err) => console.log(err))
+    })
+}
+
+module.exports = connectDB
+
+
+
+    
     
