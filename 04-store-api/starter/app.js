723b22262a305d5c12d88ee2584f8e563cf0191b
FangshengXuaJason <xufangsheng@seattleu.edu>
2021-12-18 19:56:14 -0800

    Update app.js

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,41 +1,44 @@
-require('dotenv').config();
-// require('express-async-errors');
+console.log('04 Store API')
 
-const express = require('express');
-const app = express();
+require('dotenv').config()
+require('express-async-errors') // async wrapper
+const express = require('express')
+const app = express()
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 
-const connectDB = require('./db/connect');
-const productsRouter = require('./routes/products');
-
-const notFoundMiddleware = require('./middleware/not-found');
-const errorMiddleware = require('./middleware/error-handler');
+// import error middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware =  require('./middleware/error-handler')
 
 // middleware
-app.use(express.json());
+app.use(express.json())
 
-// routes
+//  routes
 
 app.get('/', (req, res) => {
-  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
-});
+    res.send('<h1> Store API </h1> <a href="/api/v1/products"> products route</a>')
+})
 
-app.use('/api/v1/products', productsRouter);
+// product route
+app.use('/api/v1/products', productsRouter)
 
-// products route
+// handles errors
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
 
-app.use(notFoundMiddleware);
-app.use(errorMiddleware);
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 const start = async () => {
-  try {
-    // connectDB
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () => console.log(`Server is listening port ${port}...`));
-  } catch (error) {
-    console.log(error);
-  }
-};
+    try {
+        // connect DB
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening at port ${port}...`))
+        // app.listen(port, () => console.log(`Server is listening port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-start();
+start()
+
